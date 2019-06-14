7269519b7bda05ecaadc4788dd782e8b565f15d8
kakeKrg <aliakbar.kairat@gmail.com>
2019-06-14 21:17:50 +0600

    entry in website

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -2,18 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Modal from './Modal';
+import { threadId } from 'worker_threads';
+
+const users = [
+  {name: "admin", password: "qwe123" }
+];
 
 class Header extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { showModal: false, userId: '', userPassword: ''};
+    this.state = { showModal: false, adminEntry: false, userEntry: false, userId: '', userPassword: '' };
 
     this.onClick = this.onClick.bind(this);
 
     this.exiteModalWindow = this.exiteModalWindow.bind(this);
 
     this.entryInWebsite = this.entryInWebsite.bind(this);
+
+    this.userId = this.userId.bind(this);
+
+    this.userPassword = this.userPassword.bind(this);
   }
 
   onClick() {
@@ -25,8 +34,22 @@ class Header extends React.Component {
     this.setState({ showModal: false });
   }
 
-  entryInWebsite(e) {
+  userId(e) {
+    this.setState({ userId: e.target.value });
+  }
 
+  userPassword(e) {
+    this.setState({ userPassword: e.target.value });
+  }
+
+  entryInWebsite() {
+    users.map((user) => {
+      if(user.name === this.state.userId || user.password === this.state.userPassword) {
+        if("admin" === this.state.userId) {
+          this.setState({ adminEntry: true });
+        } else { this.setState({ userEntry: true }) }
+      } else { alert("Неверный логин или пароль"); }
+    });
   }
 
   render() {
@@ -35,26 +58,30 @@ class Header extends React.Component {
         <div className="box-1"><Link to="/">SATU.KZ</Link></div>
         <div className="box-2"></div>
         <div className="box-3">
-          <form>
-            <input type="text" name="login" size="16" />
-            <input type="password" name="password" size="16" />
-            <input type="button" className="input-submit" name="entry" value="Войти" onClick={this.entryInWebsite} />
-            <input type="button" className="input-submit" name="register" value="Регистрация" onClick={this.onClick} />
-          </form>
-          <div className="formExit">
-            <nav className="profil-container">
-              <ul>
-                <li><a href="#"><i className="fas fa-user-alt"></i><span></span></a>
-                  <ul>
-                    <li><a href="#"><i className="fas fa-user-circle"></i>Личные кабинет</a></li>
-                    <li><a href="#"><i className="fas fa-shopping-basket"></i>Корзина</a></li>
-                    <li><a href="#"><i className="fas fa-cart-plus"></i>Добавить объявление</a></li>
-                    <li><a href="#"><i className="fas fa-sign-out-alt"></i>Выход</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </nav>
-          </div>
+          {!this.state.userEntry && (
+            <form>
+              <input type="text" name="login" size="16" onChange={this.userId} />
+              <input type="password" name="password" size="16" onChange={this.userPassword} />
+              <input type="button" className="input-submit" name="entry" value="Войти" onClick={this.entryInWebsite} />
+              <input type="button" className="input-submit" name="register" value="Регистрация" onClick={this.onClick} />
+            </form>
+          )}
+          {this.state.userEntry && (
+            <div className="formExit">
+              <nav className="profil-container">
+                <ul>
+                  <li><a href="#"><i className="fas fa-user-alt"></i><span></span></a>
+                    <ul>
+                      <li><a href="#"><i className="fas fa-user-circle"></i>Личные кабинет</a></li>
+                      <li><a href="#"><i className="fas fa-shopping-basket"></i>Корзина</a></li>
+                      <li><a href="#"><i className="fas fa-cart-plus"></i>Добавить объявление</a></li>
+                      <li><a href="#"><i className="fas fa-sign-out-alt"></i>Выход</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          )}
         </div>
         {this.state.showModal && (
           <Modal>
