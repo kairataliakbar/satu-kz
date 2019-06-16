f4b8f689e9e644772afb9b7334bfd2cc820d1843
kakeKrg <aliakbar.kairat@gmail.com>
2019-06-16 11:54:47 +0600

    create entry admin in satu-kz

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,27 @@
+/* eslint-disable no-alert */
+/* eslint-disable no-console */
 import React from "react";
 import { Link } from "react-router-dom";
 import Modal from "react-responsive-modal";
+
+const user = { name: "admin", password: "qwe123" };
 
 class Header extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { showModal: false, userName: "", userPassword: "" };
+    this.state = {
+      showModal: false,
+      profileModal: false,
+      userName: "",
+      userPassword: ""
+    };
 
     this.modalOpen = this.modalOpen.bind(this);
     this.modalClose = this.modalClose.bind(this);
     this.userName = this.userName.bind(this);
     this.userPassword = this.userPassword.bind(this);
+    this.entryInWebsite = this.entryInWebsite.bind(this);
   }
 
   modalOpen() {
@@ -24,18 +34,28 @@ class Header extends React.Component {
 
   userName(e) {
     this.setState({ userName: e.target.value });
-    const { userName } = this.state;
-    console.log(userName);
   }
 
   userPassword(e) {
     this.setState({ userPassword: e.target.value });
+  }
+
+  entryInWebsite() {
+    const { userName } = this.state;
     const { userPassword } = this.state;
-    console.log(userPassword);
+    if (user.name === userName && user.password === userPassword) {
+      this.setState({ userName: user.name });
+      console.log(`${userName}, ${userPassword}`);
+      this.setState({ profileModal: true });
+    } else {
+      alert("Неверный логин или пароль");
+    }
   }
 
   render() {
+    const { profileModal } = this.state;
     const { showModal } = this.state;
+    const { userName } = this.state;
     return (
       <div className="header">
         <div className="box-1">
@@ -43,68 +63,73 @@ class Header extends React.Component {
         </div>
         <div className="box-2"></div>
         <div className="box-3">
-          <form>
-            <input
-              type="text"
-              name="login"
-              size="16"
-              onChange={this.userName}
-            />
-            <input
-              type="password"
-              name="password"
-              size="16"
-              onChange={this.userPassword}
-            />
-            <input
-              type="button"
-              className="input-submit"
-              name="entry"
-              value="Войти"
-            />
-            <input
-              type="button"
-              className="input-submit"
-              name="register"
-              value="Регистрация"
-              onClick={this.modalOpen}
-            />
-          </form>
-          <div className="formExit">
-            <nav className="profil-container">
-              <ul>
-                <li>
-                  <Link to="/">
-                    <i className="fas fa-user-alt"></i>
-                    <span></span>
-                  </Link>
-                  <ul>
-                    <li>
-                      <Link to="/">
-                        <i className="fas fa-user-circle"></i>Личные кабинет
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/">
-                        <i className="fas fa-shopping-basket"></i>Корзина
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/">
-                        <i className="fas fa-cart-plus"></i>
-                        Добавить объявление
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/">
-                        <i className="fas fa-sign-out-alt"></i>Выход
-                      </Link>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </nav>
-          </div>
+          {!profileModal && (
+            <form>
+              <input
+                type="text"
+                name="login"
+                size="16"
+                onChange={this.userName}
+              />
+              <input
+                type="password"
+                name="password"
+                size="16"
+                onChange={this.userPassword}
+              />
+              <input
+                type="button"
+                className="input-submit"
+                name="entry"
+                value="Войти"
+                onClick={this.entryInWebsite}
+              />
+              <input
+                type="button"
+                className="input-submit"
+                name="register"
+                value="Регистрация"
+                onClick={this.modalOpen}
+              />
+            </form>
+          )}
+          {profileModal && (
+            <div className="formExit">
+              <nav className="profil-container">
+                <ul>
+                  <li>
+                    <Link to="/">
+                      <i className="fas fa-user-alt"></i>
+                      <span>{userName}</span>
+                    </Link>
+                    <ul>
+                      <li>
+                        <Link to="/">
+                          <i className="fas fa-user-circle"></i>Личные кабинет
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/">
+                          <i className="fas fa-shopping-basket"></i>Корзина
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/">
+                          <i className="fas fa-cart-plus"></i>
+                          Добавить объявление
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/">
+                          <i className="fas fa-sign-out-alt"></i>Выход
+                        </Link>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          )}
         </div>
         <Modal open={showModal} onClose={this.modalClose}>
           <div className="modal-class">
